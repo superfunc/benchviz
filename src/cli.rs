b4cf1484e737e32ad5cf97d2274f9b9552f4fcfc
superfunc <superfunc@users.noreply.github.com>
2019-01-12 11:32:18 -0800

    Migrate CLI specific functionality out of main.

--- src/cli.rs
+++ src/cli.rs
@@ -3,4 +3,73 @@
 // Module containing io functionality for printing info to users
 // in the CLI environment
 
+use clap::ArgMatches;
 
+// Honestly I'm probably just missing how to get clap to do this
+// behavior naturally, but for now we'll just write a little function.
+pub fn parse_bench_id<'a>(matches: &'a ArgMatches, id: &str) -> (Option<&'a str>, Option<&'a str>) {
+    match (matches.value_of("name"), matches.value_of(&id)) {
+        (Some(name), Some(run_id)) => (Some(name), Some(run_id)),
+        (None, None) => (None, None),
+        (_, _) => {
+            use colored::*;
+
+            println!(
+                "{}",
+                "Error: must supply either both <name> and <run_id> or neither. \
+                 In the case of neither, a prompt will guide you."
+                    .red()
+            );
+            std::process::exit(1);
+        }
+    }
+}
+
+// Global queries require no benchmark identifier; they speak on the global state of the program
+pub fn handle_global_query(id: &str, matches: &ArgMatches, f: &Fn() -> ()) {
+    if matches.subcommand_matches(&id).is_some() {
+        f();
+    }
+}
+
+// Benchmark queries require a valid benchmark identifier; they speak on the specifics for a benchmark.
+pub fn handle_benchmark_query(id: &str, matches: &ArgMatches, f: &Fn(&str) -> ()) {
+    if let Some(v) = matches.subcommand_matches(&id) {
+        f(v.value_of("name").unwrap());
+    }
+}
+
+pub fn handle_run_data_query(id: &str, matches: &ArgMatches, f: &Fn(&str, &crate::types::RunId) -> (), g: &Fn() -> ()) {
+    if let Some(v) = matches.subcommand_matches(&id) {
+        match parse_bench_id(&v, "run_id") {
+            (Some(name), Some(run_id)) => {
+                if let Some(parsed_run_id) = crate::io::parse_run_id(&name, &run_id) {
+                    f(&name, &parsed_run_id);
+                }
+            }
+            (None, None) => g(),
+            (_, _) => unreachable!()
+        }
+    }
+}
+
+pub fn handle_multi_run_data_query(
+    id: &str,
+    matches: &ArgMatches,
+    f: &Fn(&str, crate::types::RunId, crate::types::RunId) -> (),
+    g: &Fn() -> ()
+)
+{
+    if let Some(v) = matches.subcommand_matches(&id) {
+        match (v.value_of("name"), v.value_of("run_id_1"), v.value_of("run_id_2")) {
+            (Some(name), Some(run_id_1), Some(run_id_2)) => {
+                match (crate::io::parse_run_id(&name, &run_id_1), crate::io::parse_run_id(&name, &run_id_2)) {
+                    (Some(parsed_run_id_1), Some(parsed_run_id_2)) => f(&name, parsed_run_id_1, parsed_run_id_2),
+                    (_, _) => {}
+                }
+            }
+            (None, None, None) => g(),
+            (_, _, _) => unreachable!()
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,12 @@
 // Copyright 2018 superfunc, see license.txt for usage details.
 
+mod cli;
 mod config;
 mod git;
 mod io;
 mod types;
 
 use clap::{clap_app, ArgMatches};
-
-// Honestly I'm probably just missing how to get clap to do this
-// behavior naturally, but for now we'll just write a little function.
-fn parse_bench_id<'a>(matches: &'a ArgMatches, id: &str) -> (Option<&'a str>, Option<&'a str>) {
-    match (matches.value_of("name"), matches.value_of(&id)) {
-        (Some(name), Some(run_id)) => (Some(name), Some(run_id)),
-        (None, None) => (None, None),
-        (_, _) => {
-            use colored::*;
-
-            println!(
-                "{}",
-                "Error: must supply either both <name> and <run_id> or neither. \
-                 In the case of neither, a prompt will guide you."
-                    .red()
-            );
-            std::process::exit(1);
-        }
-    }
-}
-
-// Global queries require no benchmark identifier; they speak on the global state of the program
-fn handle_global_query(id: &str, matches: &ArgMatches, f: &Fn() -> ()) {
-    if matches.subcommand_matches(&id).is_some() {
-        f();
-    }
-}
-
-// Benchmark queries require a valid benchmark identifier; they speak on the specifics for a benchmark.
-fn handle_benchmark_query(id: &str, matches: &ArgMatches, f: &Fn(&str) -> ()) {
-    if let Some(v) = matches.subcommand_matches(&id) {
-        f(v.value_of("name").unwrap());
-    }
-}
-
-fn handle_run_data_query(id: &str, matches: &ArgMatches, f: &Fn(&str, &types::RunId) -> (), g: &Fn() -> ()) {
-    if let Some(v) = matches.subcommand_matches(&id) {
-        match parse_bench_id(&v, "run_id") {
-            (Some(name), Some(run_id)) => {
-                if let Some(parsed_run_id) = io::parse_run_id(&name, &run_id) {
-                    f(&name, &parsed_run_id);
-                }
-            }
-            (None, None) => g(),
-            (_, _) => unreachable!()
-        }
-    }
-}
-
-fn handle_multi_run_data_query(id: &str, matches: &ArgMatches, f: &Fn(&str, types::RunId, types::RunId) -> (), g: &Fn() -> ()) {
-    if let Some(v) = matches.subcommand_matches(&id) {
-        match (v.value_of("name"), v.value_of("run_id_1"), v.value_of("run_id_2")) {
-            (Some(name), Some(run_id_1), Some(run_id_2)) => {
-                match (io::parse_run_id(&name, &run_id_1), io::parse_run_id(&name, &run_id_2)) {
-                    (Some(parsed_run_id_1), Some(parsed_run_id_2)) => f(&name, parsed_run_id_1, parsed_run_id_2),
-                    (_, _) => {}
-                }
-            }
-            (None, None, None) => g(),
-            (_, _, _) => unreachable!()
-        }
-    }
-}
 
 fn main() {
     let matches = clap_app!(benchviz =>
@@ -99,10 +37,10 @@ fn main() {
     config::ensure_dependencies_available();
     config::ensure_initialized();
 
-    handle_global_query("list", &matches, &io::print_current_benchmarks);
-    handle_global_query("new", &matches, &io::create_new_individual_benchmark);
-    handle_benchmark_query("info", &matches, &io::print_individual_bench_info);
-    handle_benchmark_query("run", &matches, &io::run_individual_benchmark);
-    handle_run_data_query("remove", &matches, &io::remove_benchmark_run, &io::remove_benchmark_run_with_prompt);
-    handle_multi_run_data_query("compare", &matches, &io::print_comparison, &io::print_comparison_with_prompt);
+    cli::handle_global_query("list", &matches, &io::print_current_benchmarks);
+    cli::handle_global_query("new", &matches, &io::create_new_individual_benchmark);
+    cli::handle_benchmark_query("info", &matches, &io::print_individual_bench_info);
+    cli::handle_benchmark_query("run", &matches, &io::run_individual_benchmark);
+    cli::handle_run_data_query("remove", &matches, &io::remove_benchmark_run, &io::remove_benchmark_run_with_prompt);
+    cli::handle_multi_run_data_query("compare", &matches, &io::print_comparison, &io::print_comparison_with_prompt);
 }
